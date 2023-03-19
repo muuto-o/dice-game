d87b167889a7a8faa5c4b4a12b5ce8e5c48429c3
muuto_o <unmnkh@gmail.com>
2023-03-19 18:00:39 +0800

    toglogchiin eelj solih

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // toglogchiin eeljiig hadgalah negduger toglogch 0 hoyrdugaar toglogch 1
-var activePlayer = 1;
+var activePlayer = 0;
 
 // toglogchiin tsugluulsan onoog hadgalah
 var scores = [0, 0];
@@ -19,9 +19,37 @@ document.getElementById("current-1").textContent = 0;
 
 
 diceDom.style.display = "none";
+
 document.querySelector(".btn-roll").addEventListener("click", function(){ 
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     diceDom.style.display = "block";
     diceDom.src = "dice-" + diceNumber + ".png";
-    console.log("Shoo shidlee" + diceNumber);
+
+    if(diceNumber !== 1){
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    }else {
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+
+
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        diceDom.style.display = "none";
+    }
+
+    // document.querySelector(".btn-hold").addEventListener("click", function(){
+    //     scores[activePlayer] = roundScore;
+    //     document.getElementById("score-" + activePlayer).textContent = roundScore;
+    //     roundScore = 0;
+    //     document.querySelector(".player-0-panel").classList.toggle("active");
+    //     document.querySelector(".player-1-panel").classList.toggle("active");
+    // });
 });
+
+
+
+
