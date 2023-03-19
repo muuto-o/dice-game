68824b84822bb21936aabeaab3b3d535444e27f1
muuto_o <unmnkh@gmail.com>
2023-03-19 20:12:11 +0800

    togloomin ylah onoog 100 bolgoh

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ document.querySelector(".btn-hold").addEventListener("click", function(){
         scores[activePlayer] += roundScore;
         document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
 
-    if(scores[activePlayer] >= 10){
+    if(scores[activePlayer] >= 100){
         document.getElementById("name-" + activePlayer).textContent = "WINNER !";
         document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
         document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
