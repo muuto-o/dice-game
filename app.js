3192ad97ee944253d58a40abf78cc7363945f275
muuto_o <unmnkh@gmail.com>
2023-03-19 20:00:28 +0800

    togloom hiij duusav

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 // toglogchiin eeljiig hadgalah negduger toglogch 0 hoyrdugaar toglogch 1
-var activePlayer = 0;
+var activePlayer;
 
 // toglogchiin tsugluulsan onoog hadgalah
-var scores = [0, 0];
+var scores;
 
 // toglogchiin elljindee tugluulj bui onoo
-var roundScore = 0;
-
-// shoonii talli todorhoiloh 1-6 hoorond random too bain
-
-
+var roundScore;
 var diceDom = document.querySelector(".dice");
 
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
+// shoonii talli todorhoiloh 1-6 hoorond random too baina
+
+initGame();
+
+function initGame(){
+    activePlayer = 0;
+    scores = [0, 0];
+    roundScore = 0;
+
+    document.getElementById("score-0").textContent = 0;
+    document.getElementById("score-1").textContent = 0;
+    document.getElementById("current-0").textContent = 0;
+    document.getElementById("current-1").textContent = 0;
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+
+    
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+
+    document.querySelector(".btn-roll").style.display = "block";
+    document.querySelector(".btn-hold").style.display = "block";
+
+    document.querySelector(".player-0-panel").classList.add("active");
+
+    diceDom.style.display = "none";
+}
 
 
-diceDom.style.display = "none";
+
 
 document.querySelector(".btn-roll").addEventListener("click", function(){ 
     var diceNumber = Math.floor(Math.random() * 6) + 1;
@@ -29,26 +48,39 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
         roundScore = roundScore + diceNumber;
         document.getElementById("current-" + activePlayer).textContent = roundScore;
     }else {
-        roundScore = 0;
-        document.getElementById("current-" + activePlayer).textContent = 0;
-
-
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        
-        document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
-
-        diceDom.style.display = "none";
+        switchPlayer();
     }
-
-    // document.querySelector(".btn-hold").addEventListener("click", function(){
-    //     scores[activePlayer] = roundScore;
-    //     document.getElementById("score-" + activePlayer).textContent = roundScore;
-    //     roundScore = 0;
-    //     document.querySelector(".player-0-panel").classList.toggle("active");
-    //     document.querySelector(".player-1-panel").classList.toggle("active");
-    // });
 });
+
+
+document.querySelector(".btn-hold").addEventListener("click", function(){
+        scores[activePlayer] += roundScore;
+        document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+
+    if(scores[activePlayer] >= 10){
+        document.getElementById("name-" + activePlayer).textContent = "WINNER !";
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+        document.querySelector(".btn-roll").style.display = "none";
+        document.querySelector(".btn-hold").style.display = "none";
+        document.querySelector(".dice").style.display = "none";
+    }else switchPlayer();
+});
+
+
+document.querySelector(".btn-new").addEventListener("click", initGame);
+
+
+function switchPlayer(){
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+}
 
 
 
